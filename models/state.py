5d337404fa628109faf068c4bae65aca83ff2da1
Elvis Odinigwe <102396151+Elvino03@users.noreply.github.com>
2023-06-23 16:00:43 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,32 +1,27 @@
 #!/usr/bin/python3
-"""
-State Class from Models Module
-"""
-import os
+"""Defines the State class."""
 import models
-from models.base_model import BaseModel, Base
+from os import getenv
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column
+from models.city import City
+from sqlalchemy import String
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String, Float
-STORAGE_TYPE = os.environ.get('HBNB_TYPE_STORAGE')
 
 
 class State(BaseModel, Base):
-    """State class handles all application states"""
-    if STORAGE_TYPE == "db":
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state', cascade='delete')
-    else:
-        name = ''
+    """Represents a State"""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete")
 
+    if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
-            """
-                getter method, returns list of City objs from storage
-                linked to the current State
-            """
-            city_list = []
-            for city in models.storage.all("City").values():
+            """Get a list of all related Cities."""
+            city_l = []
+            for city in list(models.storage.all(City).values()):
                 if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+                    city_l.append(city)
+            return city_l
